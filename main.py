ccd779f6b32c1371bc5b586e2685f489dc2c5bed
Usman <usman-qureshi@outlook.com>
2022-07-02 01:22:53 -0400

    Assignment complete.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import re
 
 class Program:
 
@@ -19,6 +20,9 @@ class Program:
 
         # Check if program begins with 'program', and 'begin' and ends with 'end'
         if (list[0] == 'program' and list[1]=='begin' and list[len(list) - 1]=='end'):
+            # The statement before the 'END' keyword must NOT have a semicolon, so if it does, then raise an error
+            if (";" in list[len(list) - 2]):
+                raise SyntaxError('The program contains syntax errors.')
             del list[-1]
             del list[0:2]
 
@@ -35,7 +39,12 @@ class Program:
         # Get each individual <statement> which is separated by a ';'
         listString = ' '.join(list)
         list = listString.split(';')
-
+        # Remove any blank spaces in the list
+        try:
+            while True:
+                list.remove('')
+        except ValueError:
+            pass
         # Pass each individual statement separated by a semicolon to the statement func
         for i in list:
             i = i.strip()
@@ -44,6 +53,14 @@ class Program:
     # <statement> -> <assignment_statement> | <if_statement> | <loop_statement>
     def statement(self, listString):
         list = listString.split(' ')
+        # Remove any blank spaces in the list
+        try:
+            while True:
+                list.remove('')
+        except ValueError:
+            pass
+
+
         list[:] = (value for value in list if value != '')
         list[:] = (value for value in list if value != ' ')
         
@@ -124,6 +141,10 @@ class Program:
     # <assignment_statement> -> <variable> = <expression>
     def assignment_statement(self,list):
         listString = ' '.join(list)
+        print(listString)
+        # The statement must NOT contain ANY keywords like 'if' and 'then', because if it does then that means there was a missing semicolon for one of the statements
+        if('if' in listString or 'then' in listString):
+            raise SyntaxError('The program contains syntax errors.')
 
         if ('=' in listString):
             list = listString.split('=')
@@ -169,6 +190,7 @@ class Program:
                 self.term(i)
         elif ("+" in listString):
             list = listString.split("+")
+            
             for i in list:
                 i=i.strip()
                 self.term(i)
@@ -187,29 +209,46 @@ class Program:
             for i in identifierList:
                 if not (i.isalpha() or i.isdigit()):
                     raise SyntaxError('The program contains syntax errors.')
+                else:
+                    return True
         else:
             raise SyntaxError('The program contains syntax errors.')
 
     # <term> -> <factor> {(* | /) <factor> }
     def term(self, listString):
-
+        
+        # If the term contains a * or /
+        if ("*" in listString):
+            list = listString.split('*')
+            for i in list:
+                i=i.strip()
+                self.factor(i)
+        elif ("/" in listString):
+            list = listString.split('/')
+            for i in list:
+                i=i.strip()
+                self.factor(i)
+        #else it is simply a <factor> so we send to factor func
+        else:
+            self.factor(listString)
+  
+    # <factor> -> identifier | int_constant | (<expr>)
+    def factor(self, listString):
+        # We have to determine if it is either an identifier, int constant or expression.
+        # If the factor contains parentheses which are valid, send the content inside the parentheses to the expression func
         if ("(" in listString or ")" in listString):
             if(self.parenthesesCheck(listString)):
-                # Get the content inside the parentheses
                 insideParentheses = listString[listString.find('(')+1:listString.find(')')]
-                print(insideParentheses)
+                insideParentheses=insideParentheses.strip()
+                self.expression(insideParentheses)
             else:
                 raise SyntaxError('The program contains syntax errors.')
-        # if there are no parentheses, and there is a positive or negative sign present
-       
-  
-  
-  
-  
-  
-  
-  
-  
+        # if the factor is an int constant
+        elif (listString.isdigit()):
+            return
+        # If the factor is not a int or expression inside parentheses, then we send to the variable function to check if it is a valid identifier.
+        else:
+            self.variable(listString)
   
     # Function returns true if parentheses are balanced, and false otherwise
     def parenthesesCheck(self, string):
@@ -226,6 +265,93 @@ class Program:
             return True
         else:
             return False
+    
+
+def isIdentifier(identifier):
+    # Check if the identifier is valid:
+    # An identifier is a string that begins with a letter followed by 0 or more letters and/or digits
+    identifierList = list(identifier)
+
+    # Check if the first character in the text is a letter
+    if (identifierList[0].isalpha()):
+        for i in identifierList:
+            if not (i.isalpha() or i.isdigit()):
+                return False
+        # Return true if the identifier is able to exit the for loop
+        return True
+    else: #If the identifier does not begin with a letter or char
+        return False
+
+#Function checks if string is a keyword of the language, such as 'if' or 'loop'
+def isKeyword(operator):
+    if(operator=='if' or operator=='loop' or operator=='program' or operator=='begin' or operator=='end' or operator=='then'):
+        return True
+    else:
+        return False
+
+# Function for tokens which returns the token value
+def tokenLookup(string):
+
+    if string.isdigit(): #Int constant
+        return 10
+    elif isKeyword(string): #Reserved keyword
+        return 15
+    elif string=='=':
+        return 20
+    elif string=='+':
+        return 21
+    elif string=='-':
+        return 22
+    elif string=='*':
+        return 23
+    elif string=='/':
+        return 24
+    elif string=='(':
+        return 25
+    elif string==')':
+        return 26
+    elif isIdentifier(string): #Valid variable/identifier
+        return 11
+    else: #-1 is returned if the string matches none of the tokens
+        return -1
+
+def lexicalAnalyzer(fileName):
+    
+    list = []
+
+    # Read file and append each line to lineList if it has content and is not an empty line
+    with open(fileName, 'r') as f:
+        for line in f:
+            if line not in ['\n', '\r\n']:
+                list.append(line.strip())
+   
+    newList=[]          
+    newList = [word for line in list for word in line.split()]
+    
+    for i in newList:
+        if not i.isalnum(): # If string contains non-alphanumeric chars
+            li = re.split("(\W)", i)
+            # Strip spaces and blanks from the list
+            try:
+                while True:
+                    li.remove('')
+            except ValueError:
+                pass
+                
+            for j in li:
+                str = f"Next token is: {tokenLookup(j)} Next lexeme is {j}"
+                print(str)
+        else:
+            str = f"Next token is: {tokenLookup(i)} Next lexeme is {i}"
+            print(str)
 
 
-Program().readFile('test-program-1.txt')
+# MAIN SECTION ---------------
+
+### FOR THE TA OR INSTRUCTOR ## If you would like to change the file name, please just alter this variable with the new file name
+currentFile = 'test-program-6.txt'
+### ------------------------------------------
+
+
+Program().readFile(currentFile)
+lexicalAnalyzer(currentFile)
